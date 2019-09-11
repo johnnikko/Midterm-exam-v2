78248094e7344c85b0fda09ce760d66d306f61ce
johnnikko <nikko06albarado@gmail.com>
2019-09-11 17:42:31 +0800

    add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+  require 'faker'
+
+  category = Category.create(name: "type A")
+  category = Category.create(name: "type B")
+  category = Category.create(name: "type C")
+
+  name = Faker::Name
+  10.times do
+    user = User.create(email: "#{name}@gmail.com", password: "qwer4321")
+  end
